4462c819dc6842b11905204c8207d57bc3fdbc64
Pablo Mendez Hernandez <pablomh@gmail.com>
2020-06-04 01:35:09 +0200

    compile_clang.sh: make it actually work

--- compile_clang.sh
+++ compile_clang.sh
@@ -1,7 +1,10 @@
 #! /usr/bin/bash
 
-_cross_compile=$(realpath "$ANDROID_ROOT/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-")
-_clang_path=$(realpath "$ANDROID_ROOT/prebuilts/clang/host/linux-x86/clang-4691093/bin/")
+_clang_path=$(realpath "$ANDROID_ROOT/prebuilts/clang/host/linux-x86/clang-r353983c1/bin/")
+_cross_compile_path=$(realpath "$ANDROID_ROOT/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/")
+_cross_compile_32_path=$(realpath "$ANDROID_ROOT/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/")
+
+export PATH="$_clang_path:$_cross_compile_path:$_cross_compile_32_path:${PATH}"
 
 _defconfig=aosp_${_platform}_${_device}_defconfig
 
@@ -12,7 +15,7 @@ echo "==> Building $_defconfig"
 make $_make_vars $_defconfig
 
 echo "==> Building $_targets with clang"
-make $_make_vars CROSS_COMPILE="$_cross_compile" CC="${_clang_path}/clang" CLANG_TRIPLE=aarch64-linux-gnu $_targets
+make $_make_vars CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi- $_targets
 
 echo "==> $_targets compiled successfully"
 popd
